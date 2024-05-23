7c4e527525d5d0750e5beec2f7dccc089854c518
Name Surname <email@email.com>
2024-05-23 10:16:32 +0300

    updated some files

--- movie.js
+++ movie.js
@@ -1,5 +1,7 @@
+const currentUser = localStorage.getItem("user")
 const container = document.querySelector('.cont')
-
+const reserveButton = document.querySelector('.reserve')
+const cancelButton = document.querySelector('.cancel')
 
 let movieList = localStorage.getItem("movieList")
 movieList = JSON.parse(movieList)
@@ -19,18 +21,39 @@ function appendCurrentMovie() {
             <h1>${currentMovie.title}</h1>
             <div class="sittingPlan"></div>
             <div class="btn btn-outline-light reserve">Reserve Seats</div>
-            <div class="btn btn-outline-light cancel">Cancel reservation</div>
+            ${currentUser === "admin" ? `<div class="btn btn-outline-light cancel">Cancel reservation</div>` : ''}
         </div>
         `
 }
 function appendSeats() {
     const seats = document.querySelector('.sittingPlan')
     console.log(currentMovie.seats)
+    currentMovie.seatingPlan.forEach(seat => {
+    })
+
     for (let i = 0; i < currentMovie.seats; i++) {
         const seat = document.createElement('div')
         seat.classList.add('seat')
         seats.appendChild(seat)
     }
+    const allSeats = document.querySelectorAll('.seat')
+    allSeats.forEach((seat, index) => {
+        seat.innerText = index
+        seat.onclick = () => {
+            if (!seat.classList.contains('reserved')) {
+                allSeats.forEach(seat => seat.classList.remove('picked'))
+                seat.classList.add('picked')
+            }
+
+        }
+    })
+
+
+}
+
+function reserveSeat() {
+    const allSeats = document.querySelectorAll('.seat')
+
 }
 
 appendCurrentMovie()
