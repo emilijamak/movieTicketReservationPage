a2716ae8bcc52681d5e88214f7a9d39f22ee07e1
Name Surname <email@email.com>
2024-05-24 09:02:22 +0300

    message

--- admin.js
+++ admin.js
@@ -1,42 +1,38 @@
-import {addMovie as addMovieFunction, movieInfo} from "./module.js";
-import {movieInfo as movieInfoFunction} from "./module.js";
+import { addMovie as addMovieFunction } from "./module.js";
+import { movieInfo as movieInfoFunction} from "./module.js";
 
+const currentUser = localStorage.getItem("user");
+const optionsDiv = document.querySelector('.options');
+const movieListCont = document.querySelector('.movieListCont');
+const movieCont = document.querySelector('.movieCont');
+const goBackButton = document.querySelector(".goBack");
+const goBackButton2 = document.querySelector(".goBack2");
+const checkMovieList = document.querySelector('.movieList');
+const addMovie = document.querySelector('.createMovie');
+const addMovieCont = document.querySelector('.addMovieCont');
+console.log(currentUser);
 
-const currentUser = localStorage.getItem("user")
-const optionsDiv = document.querySelector('.options')
-const movieListCont = document.querySelector('.movieListCont')
-const movieCont = document.querySelector('.movieCont')
-const goBackButton = document.querySelector(".goBack")
-const goBackButton2 = document.querySelector(".goBack2")
-const checkMovieList = document.querySelector('.movieList')
-const addMovie = document.querySelector('.createMovie')
-const addMovieCont = document.querySelector('.addMovieCont')
-console.log(currentUser)
-let movieList = localStorage.getItem("movieList")
-movieList = JSON.parse(movieList)
-
-
-
-
+let movieList = JSON.parse(localStorage.getItem("movieList")) || [];
 
 if (currentUser === "admin") {
-    optionsDiv.style.display = "flex"
-    addMovieCont.style.display = "none"
+    optionsDiv.style.display = "flex";
+    addMovieCont.style.display = "none";
 } else {
-    optionsDiv.style.display = "none"
-    movieListCont.style.display = "flex"
-    addMovieCont.style.display = "none"
-    goBackButton.style.display = "none"
-}
-checkMovieList.onclick = () => {
-    optionsDiv.style.display = "none"
-    movieListCont.style.display = "flex"
-}
-addMovie.onclick = () => {
-    optionsDiv.style.display = "none"
-    addMovieCont.style.display = "flex"
+    optionsDiv.style.display = "none";
+    movieListCont.style.display = "flex";
+    addMovieCont.style.display = "none";
+    goBackButton.style.display = "none";
 }
 
+checkMovieList.onclick = () => {
+    optionsDiv.style.display = "none";
+    movieListCont.style.display = "flex";
+};
+
+addMovie.onclick = () => {
+    optionsDiv.style.display = "none";
+    addMovieCont.style.display = "flex";
+};
 
 function appendMovies() {
     movieCont.innerHTML = "";
@@ -62,38 +58,40 @@ function appendMovies() {
             const index = parseInt(e.target.getAttribute('data-index'), 10);
             movieList = movieList.filter((_, movieIndex) => movieIndex !== index);
             appendMovies();
-            localStorage.setItem('movieList', JSON.stringify(movieList))
+            localStorage.setItem('movieList', JSON.stringify(movieList));
         };
     });
 
-addSeatingPlan(movieList)
+    addSeatingPlan(movieList);
+
 }
+
 function goBack() {
     goBackButton.onclick = () => {
-        movieListCont.style.display = "none"
-        optionsDiv.style.display = "flex"
-    }
+        movieListCont.style.display = "none";
+        optionsDiv.style.display = "flex";
+    };
 
     goBackButton2.onclick = () => {
-        addMovieCont.style.display = "none"
-        optionsDiv.style.display = "flex"
-    }
+        addMovieCont.style.display = "none";
+        optionsDiv.style.display = "flex";
+    };
 }
-
 
 function addSeatingPlan(movieList) {
     movieList.forEach(movie => {
-        const seatingPlan = Array.from({ length: movie.seats }, (_, index) => ({ id: index + 1, reserved: false }));
-        movie.seatingPlan = seatingPlan;
-        localStorage.setItem('movieList', JSON.stringify(movieList))
+        if (!movie.seatingPlan) {
+            const seatingPlan = Array.from({ length: movie.seats }, (_, index) => ({ id: index + 1, reserved: false }));
+            movie.seatingPlan = seatingPlan;
+        }
     });
+    localStorage.setItem('movieList', JSON.stringify(movieList));
 }
-
 
 console.log(movieList);
 
 appendMovies();
-goBack()
-addMovieFunction(movieList)
-movieInfo(movieList)
-console.log(movieList)
+goBack();
+addMovieFunction(movieList);
+movieInfoFunction(movieList);
+console.log(movieList);
